d157f5046a87dadac4b85830a56aa4ed6ff72d75
Jaimin Suthar <85049765+Jaimin78@users.noreply.github.com>
2023-03-09 11:01:14 +0000

    Login Authentication endpoint created

--- routes/auth.js
+++ routes/auth.js
@@ -26,7 +26,7 @@ router.post('/createuser', [
     try{
       let user = await User.findOne({email: req.body.email})
       if(user){
-        return res.status(400).json({error: "A user with this email already exists"})
+        return res.status(400).json({error: "Wrong Credential"})
       }
       
       const salt = await bcrypt.genSalt(10);
@@ -48,8 +48,50 @@ router.post('/createuser', [
       
     }catch(error){
        console.log(error.message);
-       res.status(500).send("Some error occurred")
+       res.status(500).send("Server side Error")
     }
+})
+
+
+
+//POST Req to Login Validation: /api/auth/login
+router.post('/login', [
+    body('email', 'Wrong Credential').isEmail(),
+    body('password', 'Wrong Credential').exists()
+],async (req,res) => {
+  //If there are error return bed requests and error
+  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(
+          { 
+            errors: errors.array() 
+          });
+    }
+    const {email, password} = req.body;
+    try{
+      let user = await User.findOne({email});
+      if(!user){
+        return res.status(400).json({Error:"Please try to login with correct Credential"})
+      }
+      let passwordCompare = await bcrypt.compare(password, user.password)
+      if(!passwordCompare){     
+        return res.status(400).json({Error:"Please try to login with correct Credential"})
+      }
+      
+      const data= {
+        user:{
+          id:user.id
+        }
+      }      
+      const authToken = jwt.sign(data, JWT_SECRET)
+      
+      res.json({authToken})
+      
+    }catch(error){
+       console.log(error.message);
+       res.status(500).send("Server side Error")
+    }
+  
 })
 
 module.exports = router
