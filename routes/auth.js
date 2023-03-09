16c43ea3d083448e44ce827022685a489cd1bde1
Jaimin Suthar <85049765+Jaimin78@users.noreply.github.com>
2023-03-09 03:55:55 +0000

    Converting Password into Hash and generating Web Token

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const router = express.Router();
+
+//SECRET TOKEN
+const JWT_SECRET = 'jaiminiswèbdev'
 
 //Post req to create a user : /api/auth/createuser without sign up
 router.post('/createuser', [
@@ -23,12 +28,24 @@ router.post('/createuser', [
       if(user){
         return res.status(400).json({error: "A user with this email already exists"})
       }
+      
+      const salt = await bcrypt.genSalt(10);
+      const securePassword = await bcrypt.hash(req.body.password, salt);
+      
+      //create user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
-      })
-      res.json(user)
+        password: securePassword,
+      });
+      const data= {
+        user:{
+          id:user.id
+        }
+      }
+      const authToken = jwt.sign(data, JWT_SECRET)
+      res.json({authToken})
+      
     }catch(error){
        console.log(error.message);
        res.status(500).send("Some error occurred")
