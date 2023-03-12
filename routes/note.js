935ca54e2560a7116c838d32ae351c4043b81512
Jaimin Suthar <85049765+Jaimin78@users.noreply.github.com>
2023-03-12 17:05:16 +0000

    Update notes endpoint created

--- routes/note.js
+++ routes/note.js
@@ -44,4 +44,35 @@ router.post('/addnote', fetchuser, [
    }
 })
 
+
+//ROUTE 3: PUT Req to update notes : "api/note/updatenote" login required
+router.put('/updatenote/:id', fetchuser, async (req,res) => {
+   try {
+     const { title, description, tag } = req.body;
+
+     //Create a new note
+     const newNote = {};
+     if(title){ newNote.title = title }
+     if(description){ newNote.description = description }
+     if(tag){ newNote.tag = tag }
+     
+     //Find note to be updated 
+     let note = await Note.findById(req.params.id);
+     if(!note){ return res.status(404).send("Not found"); }
+
+     //Matching existing user id with logged In user id
+     if(note.user.toString() !== req.user.id){
+       return res.status(404).send("Not allowed");
+     }
+
+     note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+     res.json({note});
+     
+   } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server Error");
+   }
+})
+
+
 module.exports = router
